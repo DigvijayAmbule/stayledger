d2c0b25c0bab545b7924f78f2ba09a2e213c7a37
Kiran Fartale <kiranfartale@gmail.com>
2023-08-26 17:57:45 +0530

    updated

--- themes/stayledger/functions.php
+++ themes/stayledger/functions.php
@@ -27,6 +27,14 @@ function add_nav_menus() {
 }
 add_action('init', 'add_nav_menus');
 
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active ';
+  }
+  return $classes;
+}
 
 if (!function_exists('myfirsttheme_setup')) :
 
